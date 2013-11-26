791f59348bb0cd5c193d91630483822e2ade52c0
Laura DeCicco <ldecicco@usgs.gov>
2013-11-26 13:39:17 -0600

    Data included in package.

---
+++ R/GLRI_TCL.R
@@ -0,0 +1,52 @@
+#' Retrieval, sorting, and graphing functions for GLRI Toxic Contaminants data
+#'
+#' \tabular{ll}{
+#' Package: \tab GLRItcl\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0.0\cr
+#' Date: \tab 2013-11-26\cr
+#' License: \tab Unlimited for this package, dependencies have more restrictive licensing.\cr
+#' Copyright: \tab This software is in the public domain because it contains materials
+#' that originally came from the United States Geological Survey, an agency of
+#' the United States Department of Interior. For more information, see the
+#' official USGS copyright policy at
+#' http://www.usgs.gov/visual-id/credit_usgs.html#copyright\cr
+#' LazyLoad: \tab yes\cr
+#' }
+#'
+#' Collection of functions to help retrieve, sort, and plot GLRI data.
+#'
+#' @name GLRItcl-package
+#' @docType package
+#' @author Laura De Cicco \email{ldecicco@@usgs.gov}
+#' @keywords data, retrieval
+NULL
+
+#' Example Data included in GLRItcl
+#'
+#' Example data pulled from water quality portal on Oct 25, 2013. New sites were added.
+#' @name QWPortalGLRI
+#' @docType data
+#' @keywords water quality data
+NULL
+#' Parameter code groups included in GLRItcl. Updated Nov. 4, 2013 with waste water tracers and endocrine disruptors.
+#'
+#' Group names, parameter codes, and class.
+#' @name pcodeINFO
+#' @docType data
+#' @keywords parameter code
+NULL
+#' Parameter code information for OWCs included in GLRItcl
+#'
+#' Group names, parameter codes, remark_cd information, and class.
+#' @name pcode_groups
+#' @docType data
+#' @keywords parameter code
+NULL
+#' Results from station search included in GLRItcl, updated Oct. 25, 2013. Some new sites added, as well as approximate land use information.
+#'
+#' Raw data pulled from NWIS
+#' @name stationINFO
+#' @docType data
+#' @keywords station data
+NULL
